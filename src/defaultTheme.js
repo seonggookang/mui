9bd17377b3810e8498110460734d15e8419c62ea
Kang <ksung91889@naver.com>
2024-04-14 19:08:37 +0900

    white

--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -8,7 +8,7 @@ export let theme = createTheme({
       light: '#757ce8',
       main: '#3f50b5',
       dark: '#002884',
-      contrastText: '#fff',
+      contrastText: '#ffffff',
     },
     secondary: {
       main: '#f44336',
